2ee93fcdd2e17c1c7da0b743120a2099e198533c
Alex Povel <alex.povel@tuhh.de>
2020-04-27 18:38:44 +0200

    Add list of installed chocolatey packages and ipython alias

--- win/ps/Microsoft.PowerShell_profile.ps1
+++ win/ps/Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,9 @@ Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 # get-command is PS-equivalent to Unix which
 New-Alias which get-command
 
+# ipython shortcut
+New-Alias py ipython
+
 # git diff output uses less; it will be buggy without utf8
 $env:LESSCHARSET='UTF-8'
 
@@ -19,5 +22,11 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Agnoster
 
-# For Agnoster theme, user@host will be hidden if user==DefaultUser
+# For Agnoster theme, "user@host" will be hidden if user==DefaultUser
 $DefaultUser = "alex"
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
