a634fc648128a726ebb5dceef0b3a0136c4919c0
Alex Povel <git@alexpovel.de>
2023-08-29 15:47:25 +0200

    chore: Use long format for shell options

--- debian.sh
+++ debian.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Get directory of this script
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
