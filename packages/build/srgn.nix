e105271573438e53ef5e5069b83ef4402ad84569
Alex Povel <git@alexpovel.de>
2024-08-22 09:28:20 +0200

    feat: srgn 0.12.0 -> 0.13.1

--- packages/build/srgn.nix
+++ packages/build/srgn.nix
@@ -4,15 +4,23 @@
 rec {
   # See also https://github.com/NixOS/nixpkgs/pull/293076, adjusted here
   pname = "srgn";
-  version = "srgn-v0.12.0";
+  version = "srgn-v0.13.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "alexpovel";
     repo = pname;
     rev = version;
-    hash = "sha256-d53aSo1gzINC8WdMzjCHzU/8+9kvrrGglV4WsiCt+rM="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
+    hash = "sha256-KG5y5V+IWIAlFULnJEomNF2Q/jyKHSSJ6o83J6vlP8w="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
   };
 
-  cargoHash = "sha256-NSP/AwghyLfaZjQ/tUv8pSbxgD6Kf12In9UdXnRLE0I="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
+  cargoHash = "sha256-Zi5QIFInh/pBLAQ8l+mqRMZlcrCT2zLPOArqtdmruYI="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
+
+  nativeBuildInputs = [ pkgs.installShellFiles ];
+
+  postInstall = ''
+    for shell in bash zsh fish; do
+      installShellCompletion --cmd srgn "--$shell" <("$out/bin/srgn" --completions "$shell")
+    done
+  '';
 }
 )
