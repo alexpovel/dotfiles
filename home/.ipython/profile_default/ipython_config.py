dc4ae06d7c0d1da4f98980ddf0a685e6a23aa7d8
Alex Povel <git@alexpovel.de>
2025-05-24 21:38:57 +0200

    feat: Do not print IPython banner

--- home/.ipython/profile_default/ipython_config.py
+++ home/.ipython/profile_default/ipython_config.py
@@ -2,6 +2,7 @@
 # https://ipython.readthedocs.io/en/stable/config/intro.html#example-configuration-file
 # `c` is magically defined.
 
+c.TerminalIPythonApp.display_banner = False
 c.InteractiveShellApp.exec_files = [
-    'init.py',
+    "init.py",
 ]
