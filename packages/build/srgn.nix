743497856e9fb711e0f7186949b2dc706d84c452
Alex Povel <git@alexpovel.de>
2025-07-05 12:49:07 +0200

    chore: srgn 0.14.0

--- packages/build/srgn.nix
+++ packages/build/srgn.nix
@@ -3,16 +3,16 @@
 (pkgs.rustPlatform.buildRustPackage rec {
   # See also https://github.com/NixOS/nixpkgs/pull/293076, adjusted here
   pname = "srgn";
-  version = "srgn-v0.13.7";
+  version = "srgn-v0.14.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "alexpovel";
     repo = pname;
     rev = version;
-    hash = "sha256-JHO++d25UmYgTuSOvkZaF0rkab8B6XetHcoEchpLimk="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
+    hash = "sha256-ZWjpkClhac4VD4b/Veffb5FHGvh+oeTu3ukaOux6MG0="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
   };
 
-  cargoHash = "sha256-H0LBH8nd/uyFufrUWVyNZjn9AKJcAlsv3UVuXoM7ZGM="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
+  cargoHash = "sha256-d/wFD0kxWNOsYaY4G5P9iM85dSo0UZGSte5AqOosM2g="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
 
   nativeBuildInputs = [ pkgs.installShellFiles ];
 
