784e972ecce88c68340d2530775f3fc32be9a444
Alex Povel <git@alexpovel.de>
2025-12-20 19:25:25 +0100

    fix(hson): Use upstream version, 0.11.5
    Has the required fixes now.

--- packages/build/headson.nix
+++ packages/build/headson.nix
@@ -2,25 +2,19 @@
 
 (pkgs.rustPlatform.buildRustPackage rec {
   pname = "headson";
-  version = "0.11.4-patched";
+  version = "0.11.5";
 
   src = pkgs.fetchFromGitHub {
-    owner = "alexpovel";
+    owner = "kantord";
     repo = pname;
-    rev = "a1a0e7dc3883b11f0ad48d6eea68adec68b76cfe";
-    hash = "sha256-tSQGPRDS7j1l/hBBovNOs8DJl6t6rAS4NSGFe6yniDE="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
+    rev = "headson-v${version}";
+    hash = "sha256-zR3Fmv4UQ2uLG6bdrtf3qIcV1wvHIbnH3cItWrY5R9o="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
   };
 
-  cargoHash = "sha256-oDCvjkqq6dEiz1Pxj4mN17Z3t2Ul+KDFDZvjEnuceW4="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
-
-  # Tests rely on temporary files and on macOS, looks like they write to `$HOME`?
-  # Failing test:
-  # https://github.com/kantord/headson/blob/a15c1bdb6fc848ea562d3498a323946a772e595d/tests/fileset_ordering.rs#L80
-  doCheck = false;
+  cargoHash = "sha256-+OiqYOpHf8Gdx6Q7Qi8p+5xjMT3sujtcIosIIxGDWnQ="; # On update: replace w/ `pkgs.lib.fakeHash`, run, let it fail and take hash from error message
 
   nativeBuildInputs = [
     pkgs.installShellFiles
-    pkgs.perl # git2 dependency builds openssl (unnecessarily) which needs perl
   ];
 
   postInstall = ''
