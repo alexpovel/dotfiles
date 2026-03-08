55dc503fa9d535c53e308459faf574aee31b5a55
Alex Povel <git@alexpovel.de>
2026-03-08 20:09:47 +0100

    chore(jj): Remove manual alias, use `builtin_draft_commit_description_with_diff`
    Pretty close to what I did manually, but now built-in

--- home.nix
+++ home.nix
@@ -311,20 +311,7 @@ in
           };
 
           templates = {
-            # Emulate `git commit --verbose`, which is neat for e.g. IDE autocomplete
-            # when writing commit messages.
-            draft_commit_description = ''
-              concat(
-                description,
-                surround(
-                  "\nJJ: This commit contains the following changes:\n", "",
-                  indent("JJ:     ", diff.stat(72)),
-                ),
-                "\nJJ: ignore-rest\n",
-                diff.git(),
-              )
-            '';
-
+            draft_commit_description = "builtin_draft_commit_description_with_diff";
             git_push_bookmark = "'${user}/' ++ change_id.short()";
           };
 
